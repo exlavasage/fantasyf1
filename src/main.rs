f4c535402d5fb8d9db563e88c5be515bddf0e7cd
Alex Savage <alex.bumstead@gmail.com>
2025-01-31 23:23:49 +0000

    Add results

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,39 @@
+use clap::Parser;
+
 use fantasyf1::puller;
 
-#[tokio::main]  
+#[derive(Parser)]
+struct Args {
+    #[clap(short, long)]
+    year: Option<u32>,
+    #[clap(short, long)]
+    results: bool,
+}
+
+#[tokio::main]
 async fn main() {
-    let races = puller::get_races(None).await.expect("Failed to get races");
-    assert!(!races.is_empty(), "Races should not be empty");
-    for race in races {
-        println!("Race: {}, Date: {}, QualiData: {} Location: {}, {}",
-            race.race_name, race.date, race.get_quali_date(), race.circuit.location.locality, race.circuit.location.country);
+    let args = Args::parse();
+
+    if args.results {
+        let results = puller::get_race_results(args.year, None)
+            .await
+            .expect("Failed to get results");
+
+        println!("{}", serde_json::to_string_pretty(&results).unwrap());
+    } else {
+        let races = puller::get_races(args.year)
+            .await
+            .expect("Failed to get races");
+
+        for race in races {
+            println!(
+                "Race: {}, Date: {}, QualiData: {} Location: {}, {}",
+                race.race_name,
+                race.date,
+                race.get_quali_date(),
+                race.circuit.location.locality,
+                race.circuit.location.country
+            );
+        }
     }
 }
--- src/puller/mod.rs
+++ src/puller/mod.rs
@@ -1,63 +1,95 @@
 use anyhow::Result;
-use serde::Deserialize;
 use chrono::NaiveDate;
+use serde::{Deserialize, Serialize};
+use serde_alias::serde_alias;
 
 const BASE_URL: &str = "https://api.jolpi.ca/ergast/f1";
 
-#[derive(Deserialize, Debug)]
+// TODO some of these fields can be flatted
+// https://crates.io/crates/serde_flat_path
+
+#[serde_alias(CamelCase, PascalCase)]
+#[derive(Deserialize, Debug, Clone, Serialize)]
 pub struct Race {
-    #[serde(alias = "raceName")]
     pub race_name: String,
     pub date: NaiveDate,
-    #[serde(alias = "Circuit")]  
     pub circuit: Circuit,
-    #[serde(alias = "Qualifying")]
-    pub qualifying: Session,
-    #[serde(alias = "SprintQualifying")]
+    pub qualifying: Option<Session>,
     pub sprint_qualifying: Option<Session>,
+    #[serde(default)]
+    pub results: Vec<RaceResult>,
 }
 
 impl Race {
     /// Get the earliest of the qualifying dates
     pub fn get_quali_date(&self) -> NaiveDate {
-        self.sprint_qualifying.as_ref().unwrap_or(&self.qualifying).date.clone()
+        self.sprint_qualifying
+            .as_ref()
+            .unwrap_or(&self.qualifying.as_ref().unwrap())
+            .date
+            .clone()
     }
 }
 
-#[derive(Deserialize, Debug)]
+#[serde_alias(CamelCase, PascalCase)]
+#[derive(Deserialize, Debug, Clone, Serialize)]
+pub struct RaceResult {
+    pub position: String,
+    pub driver: Driver,
+    pub constructor: Constructor,
+    pub time: Option<Time>,
+}
+
+#[serde_alias(CamelCase, PascalCase)]
+#[derive(Deserialize, Debug, Clone, Serialize)]
+pub struct Driver {
+    pub given_name: String,
+    pub family_name: String,
+}
+
+#[derive(Deserialize, Debug, Clone, Serialize)]
+pub struct Constructor {
+    pub name: String,
+}
+
+#[derive(Deserialize, Debug, Clone, Serialize)]
+pub struct Time {
+    pub time: String,
+}
+
+#[derive(Deserialize, Debug, Clone, Serialize)]
 pub struct Session {
     pub date: NaiveDate,
 }
 
-#[derive(Deserialize, Debug)]
+#[serde_alias(CamelCase, PascalCase)]
+#[derive(Deserialize, Debug, Clone, Serialize)]
 pub struct Circuit {
-    #[serde(alias = "circuitName")]
     pub circuit_name: String,
-    #[serde(alias = "Location")]  
     pub location: Location,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone, Serialize)]
 pub struct Location {
     pub locality: String,
     pub country: String,
 }
 
+#[serde_alias(CamelCase, PascalCase)]
 #[derive(Deserialize, Debug)]
 struct RaceTable {
-    #[serde(alias = "Races")]
     races: Vec<Race>,
 }
 
+#[serde_alias(CamelCase, PascalCase)]
 #[derive(Deserialize, Debug)]
 struct MRData {
-    #[serde(alias = "RaceTable")]
     race_table: RaceTable,
 }
 
 #[derive(Deserialize, Debug)]
 struct ApiResponse {
-    #[serde(alias = "MRData")] 
+    #[serde(alias = "MRData")]
     mr_data: MRData,
 }
 
@@ -69,11 +101,25 @@ pub async fn get_races(year: Option<u32>) -> Result<Vec<Race>> {
     };
 
     let response = reqwest::get(&url).await?;
-    let json : serde_json::Value = response.json().await?;
+    let json: serde_json::Value = response.json().await?;
     eprintln!("{}", serde_json::to_string_pretty(&json)?);
 
-    let api_response : ApiResponse = serde_json::from_value(json)?;  
+    let api_response: ApiResponse = serde_json::from_value(json)?;
     Ok(api_response.mr_data.race_table.races)
+}
+
+pub async fn get_race_results(year: Option<u32>, round: Option<u32>) -> Result<Race> {
+    let year = year.map_or("current".to_string(), |y| y.to_string());
+    let round = round.map_or("last".to_string(), |r| r.to_string());
+
+    let url = format!("{}/{}/{}/results.json", BASE_URL, year, round);
+
+    let response = reqwest::get(&url).await?;
+    let json: serde_json::Value = response.json().await?;
+    eprintln!("{}", serde_json::to_string_pretty(&json)?);
+
+    let api_response: ApiResponse = serde_json::from_value(json)?;
+    Ok(api_response.mr_data.race_table.races[0].clone())
 }
 
 #[cfg(test)]
@@ -86,8 +132,13 @@ mod tests {
             Ok(races) => {
                 assert!(!races.is_empty(), "Races should not be empty");
                 for race in races {
-                    println!("Race: {}, Date: {}, Location: {}, {}",
-                        race.race_name, race.date, race.circuit.location.locality, race.circuit.location.country);
+                    println!(
+                        "Race: {}, Date: {}, Location: {}, {}",
+                        race.race_name,
+                        race.date,
+                        race.circuit.location.locality,
+                        race.circuit.location.country
+                    );
                 }
             }
             Err(e) => panic!("Error fetching races: {}", e),
